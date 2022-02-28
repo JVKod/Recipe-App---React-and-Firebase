428bba366ca563d631400fd26e7da3ad6a2d2a78
JVKod <93686953+JVKod@users.noreply.github.com>
2022-02-27 20:16:42 -0500

    added search bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header';
-import { auth, db } from 'firebaseConfig';
+import { auth, db } from './firebaseConfig';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <SearchBar></SearchBar>
     </div>
   );
 }
---
+++ src/components/SearchBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './SearchBar.css';
+
+function SearchBar() {
+    return (
+      <div>
+        <input type='text'placeholder="Look for a recipe..."/>
+      </div>
+    );
+  }
+  
+  export default SearchBar;
