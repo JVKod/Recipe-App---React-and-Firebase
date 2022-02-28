b7d8ecf5767d1ea611d899d6b3261f055dcbe999
JVKod <93686953+JVKod@users.noreply.github.com>
2022-02-28 00:15:56 -0500

    added recipe component plus code to retrieve recipes from firestore db and convert to objects

---
+++ src/components/recipe.js
@@ -0,0 +1,8 @@
+export const Recipe = (name, ingredients, amounts, steps) => { 
+    return {
+        name: name, 
+        ingredients: ingredients, 
+        amounts: amounts, 
+        steps: steps 
+    } 
+}
