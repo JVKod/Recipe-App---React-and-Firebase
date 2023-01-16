fe8d8abad1670a7cd84562381b323325f91fef0a
Justin K <93686953+JVKod@users.noreply.github.com>
2023-01-16 15:29:30 -0500

    Delete src directory

--- src/components/recipe.js
+++
@@ -1,9 +0,0 @@
-export const Recipe = (name, ingredients, amounts, steps, tags) => { 
-    return {
-        name: name, 
-        ingredients: ingredients, 
-        amounts: amounts, 
-        steps: steps, 
-        tags: tags
-    } 
-}
